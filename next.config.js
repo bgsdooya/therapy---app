c77f31794047d93356986a1054c174be657c8f43
bgsdooya <bgsdooya@gmail.com>
2026-03-20 12:49:26 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
         ],
       },
       {
+        source: "/firebase-messaging-sw.js",
+        headers: [
+          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },
+          { key: "Content-Type", value: "application/javascript; charset=utf-8" },
+        ],
+      },
+      {
         source: "/manifest.json",
         headers: [
           { key: "Content-Type", value: "application/manifest+json" },
